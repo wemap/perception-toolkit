a81f8a8e138b5a6526b7d64c6b61376ddc35c579
Matt Small <mbsmall@google.com>
2019-03-28 22:43:20 -0700

    Change to ASL

--- lib/src/elements/no-support-card/no-support-card.template.d.ts
+++ lib/src/elements/no-support-card/no-support-card.template.d.ts
@@ -1,11 +1,18 @@
 /**
  * @license
- * Copyright (c) 2019 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 export declare const styles = "\n:host {\n  --baseline: 8px;\n  --background: #FFF;\n  --borderRadius: 4px;\n  --color: #333;\n  --fontFamily: 'Arial', 'Helvetica', sans-serif;\n  --padding: calc(var(--baseline) * 2)\n      calc(var(--baseline) * 4)\n      calc(var(--baseline) * 2)\n      calc(var(--baseline) * 3);\n\n  position: relative;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  max-width: 80vw;\n  text-align: center;\n}\n\n#container {\n  padding: var(--padding);\n  border-radius: var(--borderRadius);\n  font-family: var(--fontFamily);\n  background: var(--background);\n  color: var(--color);\n}\n";
 export declare const html = "<div id=\"container\"></div>";
--- lib/src/elements/stream-capture/stream-capture.template.d.ts
+++ lib/src/elements/stream-capture/stream-capture.template.d.ts
@@ -1,11 +1,18 @@
 /**
  * @license
- * Copyright (c) 2019 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 export declare const styles: string;
 export declare const html = "<svg id=\"reticle\" viewBox=\"0 0 133 100\"\n    xmlns=\"http://www.w3.org/2000/svg\">\n  <mask id=\"reticle-cut-out\">\n    <rect id=\"reticle-cut-out-outer\" width=\"133\" height=\"100\" x=\"0\" y=\"0\"\n        fill=\"#FFF\" />\n    <rect id=\"reticle-cut-out-inner\" x=\"24\" y=\"8\" width=\"85\" height=\"85\" rx=\"2\"\n        ry=\"2\" fill=\"#000\" />\n  </mask>\n  <rect id=\"reticle-box\" width=\"133\" height=\"100\" x=\"0\" y=\"0\"\n      fill=\"rgba(0,0,0,0.4)\" mask=\"url(#reticle-cut-out)\" />\n</svg>\n<button id=\"close\">Close</button>\n";
