f894ff2e22a2efaba54bb624c5751602081c1e06
Michal Mocny <mmocny@gmail.com>
2019-04-12 09:40:14 -0400

    WIP: Cache auxilliary pageMetadata

--- perception-toolkit/meaning-maker.ts
+++ perception-toolkit/meaning-maker.ts
@@ -24,8 +24,9 @@ import { GeoCoordinates } from '../src/artifacts/schema/geo-coordinates';
 import { JsonLd } from '../src/artifacts/schema/json-ld';
 import { LocalArtifactStore } from '../src/artifacts/stores/local-artifact-store';
 import { fetchAsDocument } from '../src/utils/fetch-as-document';
+import { ContentMetadataManager } from './content-metadata-manager';
 
-type ShouldFetchArtifactsFromCallback = ((url: URL) => boolean) | string[];
+type ShouldFetchArtifactsFromCallback = ((url: URL) => boolean);
 
 /*
  * MeaningMaker binds the Artifacts components with the rest of the Perception Toolkit.
@@ -41,6 +42,7 @@ export class MeaningMaker {
   private readonly artloader = new ArtifactLoader();
   private readonly artstore = new LocalArtifactStore();
   private readonly artdealer = new ArtifactDealer();
+  private readonly pageMetadataCache = new Map<URL, JsonLd>();
 
   constructor() {
     this.artdealer.addArtifactStore(this.artstore);
@@ -55,7 +57,9 @@ export class MeaningMaker {
   }
 
   /**
-   * Load artifact content for initial set.
+   * Load artifact content for a json-ld file.
+   * This could just a small set of Artifacts defined in an external script, but often this is used to load a large
+   * set of Artifacts for an entire site ("artifact sitemap").
    */
   async loadArtifactsFromJsonldUrl(url: URL) {
     const artifacts = await this.artloader.fromJsonUrl(url);
@@ -63,7 +67,9 @@ export class MeaningMaker {
   }
 
   /**
-   * Load artifact content from url on same origin, usually discovered from environment.
+   * Load artifact content from an html file.
+   * This could be used to manualy crawl a site, but usually this is just used to index a single page, for which a URL
+   * was discovered at runtime.  E.g. QRCode with URL in it, or an OCR-ed URL on a poster.
    */
   async loadArtifactsFromHtmlUrl(url: URL) {
     const artifacts = await this.artloader.fromHtmlUrl(url);
@@ -71,14 +77,37 @@ export class MeaningMaker {
     return artifacts;
   }
 
-  async markerFound(marker: Marker, shouldFetchArtifactsFrom?: ShouldFetchArtifactsFromCallback):
-      Promise<NearbyResultDelta> {
+  /*
+   * This will extract page metadata
+   */
+  async getPageMetadata(url: URL, doc?: Document): Promise<JsonLd | null> {
+    if (this.pageMetadataCache.has(url)) {
+      return this.pageMetadataCache.get(url) as JsonLd;
+    }
+    return this.tryExtractPageMetadata(url)
+  }
+
+
+  /*
+   * Let meaning-maker know that a new marker was discovered in the environment.
+   * Returns a list of results based on this new information.
+   */
+  async markerFound(marker: Marker, shouldFetchArtifactsFrom?: ShouldFetchArtifactsFromCallback | string[]):
+    Promise<NearbyResultDelta> {
+    if (!shouldFetchArtifactsFrom) {
+      // If there's no callback provided, match to current origin.
+      shouldFetchArtifactsFrom = (url: URL) => url.origin === window.location.origin;
+    } else if (Array.isArray(shouldFetchArtifactsFrom)) {
+      // If an array of strings, remap it.
+      const origins = shouldFetchArtifactsFrom;
+      shouldFetchArtifactsFrom = (url: URL) => !!origins.find(o => o === url.origin);
+    }
+
     // If this marker is a URL, try loading artifacts from that URL
-
     const url = this.checkIsFetchableURL(marker.value, shouldFetchArtifactsFrom);
-
     if (url) {
       // TODO: fetchAsDocument here, pass that into loader.  Reuse doc below.
+
       const artifacts = await this.loadArtifactsFromHtmlUrl(url);
     }
 
@@ -87,7 +116,7 @@ export class MeaningMaker {
     // If any results have URL-only content -- try loading from the page itself
     for (const result of results.found) {
       // TODO: if this result is the same URL as the one we just fetched, re-use the doc.
-      await this.attemptEnrichContentWithPageMetadata(result);
+      await this.attemptEnrichContentWithPageMetadata(result, shouldFetchArtifactsFrom);
     }
     // Do not enrich lost content.  Should only need target info.
 
@@ -109,16 +138,7 @@ export class MeaningMaker {
   }
 
   private checkIsFetchableURL(potentialUrl: string,
-                              shouldFetchArtifactsFrom?: ShouldFetchArtifactsFromCallback): URL | null {
-    if (!shouldFetchArtifactsFrom) {
-      // If there's no callback provided, match to current origin.
-      shouldFetchArtifactsFrom = (url: URL) => url.origin === window.location.origin;
-    } else if (Array.isArray(shouldFetchArtifactsFrom)) {
-      // If an array of strings, remap it.
-      const origins = shouldFetchArtifactsFrom;
-      shouldFetchArtifactsFrom = (url: URL) => !!origins.find(o => o === url.origin);
-    }
-
+                              shouldFetchArtifactsFrom: ShouldFetchArtifactsFromCallback): URL | null {
     try {
       // This will throw if potentialUrl isn't a valid URL.
       // Do not supply a base url argument, since we do not want to support relative URLs,
@@ -133,23 +153,34 @@ export class MeaningMaker {
     return null;
   }
 
-  private async tryExtractPageMetadata(potentialUrl: string): Promise<JsonLd | null> {
-    const url = this.checkIsFetchableURL(potentialUrl);
+  private async tryExtractPageMetadata(
+        potentialUrl: string,
+        shouldFetchArtifactsFrom: ShouldFetchArtifactsFromCallback
+      ): Promise<JsonLd | null> {
+    const url = this.checkIsFetchableURL(potentialUrl, shouldFetchArtifactsFrom);
     if (url) {
-      const doc = await fetchAsDocument(url);
-      if (doc) {
-        const pageMetadata = await extractPageMetadata(doc, url);
-        return pageMetadata;
-      }
+      this.extractPageMetadata(url);
     }
     return null;
   }
 
-  private async attemptEnrichContentWithPageMetadata(result: NearbyResult): Promise<NearbyResult> {
+  private async extractPageMetadata(url: URL): Promise<JsonLd | null> {
+    const doc = await fetchAsDocument(url);
+    if (doc) {
+      const pageMetadata = await extractPageMetadata(doc, url);
+      return pageMetadata;
+    }
+    return null;
+  }
+
+  private async attemptEnrichContentWithPageMetadata(
+        result: NearbyResult,
+        shouldFetchArtifactsFrom: ShouldFetchArtifactsFromCallback
+      ): Promise<NearbyResult> {
     if (result.content) {
       if (typeof result.content === 'string') {
         // if arContent is a string, try to treat it as a URL
-        const pageMetadata = await this.tryExtractPageMetadata(result.content);
+        const pageMetadata = await this.tryExtractPageMetadata(result.content, shouldFetchArtifactsFrom);
         if (pageMetadata) {
           // Override the string URL with the metadata object
           result.content = pageMetadata;
@@ -157,7 +188,7 @@ export class MeaningMaker {
       } else if ('url' in result.content && !('name' in result.content)) {
         // if arContent has a 'url' property, but lacks properties, check the page for missing metadata.
         // For now, make sure the @type's match exactly, so we only ever expand metadata.
-        const pageMetadata = await this.tryExtractPageMetadata(result.content.url);
+        const pageMetadata = await this.tryExtractPageMetadata(result.content.url, shouldFetchArtifactsFrom);
         if (pageMetadata && pageMetadata['@type'] === result.content['@type']) {
           // Use the new metadata object, but keep all the existing property values.
           result.content = Object.assign(pageMetadata, result.content);
